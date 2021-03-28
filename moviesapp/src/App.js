410f9f730dfa07f8b90ab0a3330b18de2586fc41
rraj7 <rraj7@uic.edu>
2021-03-28 02:50:15 -0400

    Added header.js

---
+++ moviesapp/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SearchAppBar from "./Component/Header/header";
+
+function App() {
+  return (
+    <div className="App">
+    <SearchAppBar/>
+    </div>
+  );
+}
+
+export default App;
